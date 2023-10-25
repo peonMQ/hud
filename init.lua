5df73087b74aaf0f3b0a85a3b09ca9ba2be69b89
Aleksander Spro <abspro@gmail.com>
2023-10-25 18:31:20 +0200

    DI HUD dependencies to allow for integration into other scripts

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
-local hud = require 'hud'
+local mq = require 'mq'
+local configuration = require 'utils/configloader'
+local logger = require 'utils/logging'
+local hudInit = require 'hud'
 
-hud.MainLoop()
+local generalSettings = configuration("general", {scale = 1.0}, "data/HUD")
+local groupLayoutMode = configuration("grouplayout", nil, "data/HUD") or {}
+
+local hud = hudInit(generalSettings, groupLayoutMode)
+
+local function mainLoop()
+  while not hud.ShouldTerminate() do
+    hud.Update(logger)
+    mq.delay(500)
+  end
+end
